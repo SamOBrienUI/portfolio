e2af5cd0bfafd5b6dab6d6cadbd61919273f68e8
Sam <sam@Philips-MacBook-Air.local>
2025-04-09 15:56:12 -0400

    test

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: "export",
-    basePath: "/portfolio"
+    basePath: "/portfolio",
+    images: {
+        unoptimized: true
+    }
 };
 
 export default nextConfig;
